8205703dabc98690ad7c9a29f87ab1436b178f66
idith almog <almog.idith@gmail.com>
2020-04-19 21:16:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   counter=0
   board.each do |square|
-    if ! (square == "" || square == " " || square == nil)
+    if (square == "" || square == " " || square == nil)
+      counter
+    else
       counter +=1
     end
   end
