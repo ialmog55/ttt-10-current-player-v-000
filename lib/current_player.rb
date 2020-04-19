0d6590b27a60b899110b90d26c0f31de34f75630
idith almog <almog.idith@gmail.com>
2020-04-19 21:13:58 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  counter=0
+  board.each do |square|
+    if ! (square == "" || square == " " || square == nil)
+      counter +=1
+    end
+  end
+end
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
